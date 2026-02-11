e59e5ae980f58ea4094b6ddfac901aa36c38cdd5
Charchit Gupta <charchitg552@gmail.com>
2026-02-11 10:50:40 +0530

    Assignement Done

---
+++ NODEJS/Assignement/Assigenement2/frontend/src/Home.js
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div style={{ display: "flex", alignItems: "center", marginTop: "40px" }}>
+      
+      <img
+        src="https://threebestrated.in/images/ABESEngineeringCollege-Ghaziabad-UP.jpeg"
+        width="300"
+        alt="college"
+      />
+
+      <h1 style={{ marginLeft: "40px" }}>ABES College</h1>
+
+    </div>
+  );
+}
+
+export default Home;
